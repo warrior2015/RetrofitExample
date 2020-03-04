3404050497a232f11dba263035a8862a8fbdaaa1
neeraj t <n.tamrakar2010@gmail.com>
2020-03-04 00:32:36 -0800

    Added Retrofit call

--- app/src/main/java/com/neeraj/retrofitexample/MainActivity.kt
+++ app/src/main/java/com/neeraj/retrofitexample/MainActivity.kt
@@ -6,40 +6,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.neeraj.retrofitexample.data.WeatherService
 import com.neeraj.retrofitexample.data.WikiApiService
+import com.neeraj.retrofitexample.models.WeatherResponse
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     private var disposable: Disposable? = null
 
-    private val wikiApiServe by lazy {
-        WikiApiService.create("https://en.wikipedia.org/w/")
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        button_with_callable.setOnClickListener {
+        button_with_rx.setOnClickListener {
             if (editText.text.toString().isNotEmpty()) {
                 beginSearch(editText.text.toString().toString())
             }
         }
-
+        button_with_callable.setOnClickListener{ getCurrentData()}
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
 
+    // Example 1
     private fun beginSearch(searchString: String) {
+        val wikiApiServe by lazy {
+            WikiApiService.create("https://en.wikipedia.org/w/")
+        }
         disposable = wikiApiServe.hitCountCheck("query", "json", "search", searchString)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
@@ -53,4 +59,51 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         disposable?.dispose()
     }
+
+    //Example 2
+    internal fun getCurrentData() {
+        var BaseUrl = "http://api.openweathermap.org/"
+        var AppId = "2e65127e909e178d0af311a81f39948c"
+        var lat = "35"
+        var lon = "139"
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val service = retrofit.create(WeatherService::class.java)
+        val call = service.getCurrentWeatherData(lat, lon, AppId)
+        call.enqueue(object : Callback<WeatherResponse> {
+            override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+                if (response.code() == 200) {
+                    val weatherResponse = response.body()!!
+
+                    val stringBuilder = "Country: " +
+                            weatherResponse.sys!!.country +
+                            "\n" +
+                            "Temperature: " +
+                            weatherResponse.main!!.temp +
+                            "\n" +
+                            "Temperature(Min): " +
+                            weatherResponse.main!!.temp_min +
+                            "\n" +
+                            "Temperature(Max): " +
+                            weatherResponse.main!!.temp_max +
+                            "\n" +
+                            "Humidity: " +
+                            weatherResponse.main!!.humidity +
+                            "\n" +
+                            "Pressure: " +
+                            weatherResponse.main!!.pressure
+
+                    textView.text = stringBuilder
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                textView.text = t.message
+            }
+        })
+    }
+
 }
